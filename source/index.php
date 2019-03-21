1227cc6946bc6d3951edbc7bb5b9c2015226b146
Suraj Sanwal <sanwal.suraj12@gmail.com>
2019-03-22 00:07:32 +0530

    code update with menu

--- source/index.php
+++ source/index.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Bootstrap</title>	
-		<meta name="viewport" content="width=device-width, initial-scale=1">	
-		<link href="../assets/css/bootstrap.min.css"rel="stylesheet">
-
-	</head>
-	<body>
-		<div class="container">
-			<div class="row">
- 				 <div class="col-sm-6">
- 				 	<blockquote>
-   					 <p>For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.
-   					 </p>
-   					 <footer>From WWF's website</footer>
- 					 </blockquote>
- 				</div>
- 				 <div class="col-sm-6">2</div>
-			</div>
-			<div class="row">
-  				<div class="col-sm-4">3</div>
-  				<div class="col-sm-4">4</div>
-  				<div class="col-sm-4">5</div>
-			</div>
-		</div>
-	</body>
-</html>
+<?php 
+	header("location:./views");
+?>
